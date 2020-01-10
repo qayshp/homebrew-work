e1d8062198be4dbe71527e55833dcb9dbef3ec7b
Qays H. Poonawala <qayshp@gmail.com>
2020-01-10 12:58:32 -0800

    Update layer0 to 0.11.1

--- Formula/layer0.rb
+++ Formula/layer0.rb
@@ -1,9 +1,9 @@
 class Layer0 < Formula
   desc "Build, Manage, and Deploy Your Application"
   homepage "http://layer0.ims.io/"
-  url "https://github.com/quintilesims/layer0/releases/download/v0.10.9/macOS.zip"
-  version "0.10.9"
-  sha256 "188434bdca7d320b4341836225850c78ec27c0ac9959ba694a4ad2e8a033662a"
+  url "https://github.com/quintilesims/layer0/releases/download/v0.11.1/macOS.zip"
+  version "0.11.1"
+  sha256 "3110dbc6e2eb16b28946d726979aa1f8e40026e056874d7745eb4fca925dc30e"
 
   depends_on "terraform"
 
